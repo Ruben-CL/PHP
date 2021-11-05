efb83db481f6f957b87c7eb6a25100fbbff06c22
Ruben-CL <78144920+Ruben-CL@users.noreply.github.com>
2021-11-05 09:26:15 +0100

    Delete error.php

--- error.php
+++
@@ -1,8 +0,0 @@
-<form method="POST" action="casino.php"> 
-<p>Error: no dispone de <?= $_SESSION['dinero']?></p>
-<p>dispone de  <?= $_SESSION['dinero']?>euros para jugar</p>
-<p>cantidad a apostar</p> <input type="number" name="dineroApostado">
-<p>par</p><input type="radio" value="par"><p>impar</p><input type="radio" value="impar">
-<input type="submit" value="apostar" name="ejecutar">
-<input type="submit" value="abandonar" name="ejecutar">
-</form>

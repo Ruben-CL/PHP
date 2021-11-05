461d7dfd49870ed5fe18faabf2f6f3b0f2a599af
Ruben-CL <78144920+Ruben-CL@users.noreply.github.com>
2021-11-05 09:27:01 +0100

    Delete archivo3.php

--- archivo3.php
+++
@@ -1,4 +0,0 @@
-
-
-<p>este es su pedido: </p>
-<textarea name="caja" id="caja" cols="15" rows=<?=count($_SESSION['frutas'])?>><?php foreach($_SESSION['frutas'] as $fruta => $cantidad):?><?=htmlspecialchars($fruta)?>&nbsp<?= htmlspecialchars($cantidad)?> <?php endforeach ?></textarea>

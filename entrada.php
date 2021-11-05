3ae45ef3cb37ff942952fa43fc3860150f9eb828
Ruben-CL <78144920+Ruben-CL@users.noreply.github.com>
2021-11-05 09:27:23 +0100

    Delete entrada.php

--- entrada.php
+++
@@ -1,5 +0,0 @@
-<form method="GET" action="casino.php"> 
-<h1>bienvenido al casino</h1>
-<p>Esta es su <?= $numvisitas ?>ª visita</p>
-<p>introduzca el dinero con el que va a jugar</p> <input type="number" name="dinero">
-</form>

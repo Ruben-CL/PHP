437acf9f9edc24568f00f68c82011ed2e7cd13f2
Ruben-CL <78144920+Ruben-CL@users.noreply.github.com>
2021-11-05 09:26:49 +0100

    Delete archivo1.php

--- archivo1.php
+++
@@ -1,5 +0,0 @@
-<form method="GET" action="FRUTERIA.php"> 
-<p>bienvenido a la fruteria del siglo XXI</p>
-<p>introduzca su nombre de cliente</p> <input type="text" name="nombre">
-<input type="submit" value="aceptar">
-</form>
